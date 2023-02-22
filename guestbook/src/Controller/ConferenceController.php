c39498baa893d097d3f25b30dbb028305bc5681e
Aurel <aureliencrest@icloud.com>
2023-02-22 13:24:21 +0000

    bootstrap

--- guestbook/src/Controller/ConferenceController.php
+++ guestbook/src/Controller/ConferenceController.php
@@ -29,7 +29,7 @@ class ConferenceController extends AbstractController
     public function index(ConferenceRepository $conferenceRepository): Response
     {
         return $this->render('conference/index.html.twig', [
-                        'conferences' => $conferenceRepository->findAll(),
+            'conferences' => $conferenceRepository->findAll(),
         ]);
 
     }
